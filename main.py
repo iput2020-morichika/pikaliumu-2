873a6de39992ddf6b8056006b16c8b20d859b006
iput2020-morichika <104192526+iput2020-morichika@users.noreply.github.com>
2023-05-20 20:48:26 +0900

    Revert "Led test"

--- main.py
+++ main.py
@@ -1,32 +1,17 @@
 """
 ファイルインポート
 """
-# from sensor import suion
-from sensor import jinkan
+from sensor import suion
 from led import led_class
 
 """
 初期化処理
 """
-class Flip:
-   def __init__(self,flip):
-      self.flip = flip
-flip = Flip(0)
+led_class
 
-jinkan.jinkan()    
+s1 = suion.temp_c
+print (s1)
 
-while True:
-  if jinkan.jinkan() == 1:  #人感センサ反応でLED点灯
-    print("点灯")
-    led_class.Gra_led.laser(flip)
-  else:
-    led_class.trun_off()    #消灯
-    print("消灯")
-
-
-# s1 = suion.temp_c
-# print (s1)
-
-# if s1 > 26.0:
-
+if s1 > 26.0:
+  led_class.loop()
   
